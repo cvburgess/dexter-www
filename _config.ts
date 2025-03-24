e753365d309b6ead4a15bcdabd7ab166c66599f4
Charles Burgess <consulting@cvburgess.com>
2025-03-24 15:16:30 -0400

    Migrate to lume 3

--- _config.ts
+++ _config.ts
@@ -1,12 +1,14 @@
 import lume from "lume/mod.ts";
+import tailwindcss from "lume/plugins/tailwindcss.ts";
 
-const BASE_URL = "https://cvburgess.com";
+const BASE_URL = "https://dexterplanner.com";
 
 const site = lume({
   location: new URL(BASE_URL),
   src: "./src",
 });
 
-site.copyRemainingFiles();
+site.use(tailwindcss());
+site.add("index.css");
 
 export default site;
