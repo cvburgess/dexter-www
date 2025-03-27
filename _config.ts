0c23892bf79b102f4c78fb6193aacfa5f1744a98
Charles Burgess <consulting@cvburgess.com>
2025-03-27 19:38:11 -0400

    Lume v3 (#1)
    * Migrate to lume 3
    
    * WIP layout

--- _config.ts
+++ _config.ts
@@ -1,12 +1,16 @@
 import lume from "lume/mod.ts";
+import nunjucks from "lume/plugins/nunjucks.ts";
+import tailwindcss from "lume/plugins/tailwindcss.ts";
 
-const BASE_URL = "https://cvburgess.com";
+const BASE_URL = "https://dexterplanner.com";
 
 const site = lume({
   location: new URL(BASE_URL),
   src: "./src",
 });
 
-site.copyRemainingFiles();
+site.use(nunjucks());
+site.use(tailwindcss());
+site.add("index.css");
 
 export default site;
